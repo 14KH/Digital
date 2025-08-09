83687cf5ced8a99e82586266505eeb10eee6be31
14KH <mentassir66@gmail.com>
2025-08-09 20:52:54 +0200

    Update App.js

--- SRC/App.js
+++ SRC/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import HamburgerMenu from './Menu';
 import './Menu.css';
 
-
 function App() {
   const [pages, setPages] = useState(['الرئيسية']);
 
@@ -13,11 +12,7 @@ function App() {
 
   return (
     <div>
-    
-    <HamburgerMenu onAdd={handleAdd} />
-    
-
-      <Menu onAdd={handleAdd} />
+      <HamburgerMenu onAdd={handleAdd} />
       <div style={{ marginTop: '20px', padding: '10px' }}>
         {pages.map((page, index) => (
           <div key={index} style={{ padding: '10px', border: '1px solid #ccc', marginBottom: '10px' }}>
