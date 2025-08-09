776ef8143b470ff4eb6cc0852bbcf29389839b1c
14KH <mentassir66@gmail.com>
2025-08-09 15:09:46 +0200

    Update App.js

--- SRC/App.js
+++ SRC/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import Menu from "./Menu";
+import React, { useState } from 'react';
+import Menu from './Menu';
+import './Menu.css';
 
-export default function App() {
+function App() {
+  const [pages, setPages] = useState(['الرئيسية']);
+
+  const handleAdd = (type) => {
+    const newPage = `صفحة ${pages.length + 1} - ${type}`;
+    setPages([...pages, newPage]);
+  };
+
   return (
-    <>
-      <Menu />
-      <h1>Welcome</h1>
-    </>
+    <div>
+      <Menu onAdd={handleAdd} />
+      <div style={{ marginTop: '20px', padding: '10px' }}>
+        {pages.map((page, index) => (
+          <div key={index} style={{ padding: '10px', border: '1px solid #ccc', marginBottom: '10px' }}>
+            {page}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
+
+export default App;
