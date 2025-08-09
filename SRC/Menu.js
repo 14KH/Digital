98c719a69276bcabb8d427b30bca2f5777ddab02
14KH <mentassir66@gmail.com>
2025-08-09 20:11:13 +0200

    Update Menu.js

--- SRC/Menu.js
+++ SRC/Menu.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Menu.css';
 
-function Menu({ onAdd }) {
+function HamburgerMenu({ onAdd }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav>
-      <ul>
-        <li><button onClick={() => onAdd('page')}>إضافة صفحة جديدة</button></li>
-        <li><button onClick={() => onAdd('section')}>إضافة قسم جديد</button></li>
-        <li><button onClick={() => onAdd('image')}>إضافة صورة جديدة</button></li>
-        <li><button onClick={() => onAdd('video')}>إضافة فيديو جديد</button></li>
-        <li><button onClick={() => onAdd('text')}>إضافة نص جديد</button></li>
-        <li><button onClick={() => onAdd('ad')}>إضافة إعلان جديد</button></li>
-      </ul>
-    </nav>
+    <>
+      <button className="hamburger-btn" onClick={() => setOpen(!open)}>
+        <div className={`bar ${open ? 'open' : ''}`}></div>
+        <div className={`bar ${open ? 'open' : ''}`}></div>
+        <div className={`bar ${open ? 'open' : ''}`}></div>
+      </button>
+
+      {open && (
+        <nav className="hamburger-menu">
+          <ul>
+            <li><button onClick={() => onAdd('page')}>إضافة صفحة جديدة</button></li>
+            <li><button onClick={() => onAdd('section')}>إضافة قسم جديد</button></li>
+            <li><button onClick={() => onAdd('image')}>إضافة صورة جديدة</button></li>
+            <li><button onClick={() => onAdd('video')}>إضافة فيديو جديد</button></li>
+            <li><button onClick={() => onAdd('text')}>إضافة نص جديد</button></li>
+            <li><button onClick={() => onAdd('ad')}>إضافة إعلان جديد</button></li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
 
-export default Menu;
+export default HamburgerMenu;
